fc8383258a03846dbd8e1c383c3ee2c737651383
neponed <nepo.ned@mpilo.africa>
2023-08-30 10:04:57 +0200

    Add project files.

---
+++ USSEnterprise.Application/Interfaces/IElevatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USSEnterprise.Application.Interfaces
+{
+    public interface IElevatorService
+    {
+        void RequestFloor(int floor);
+    }
+}
---
+++ USSEnterprise.Application/Services/ElevatorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USSEnterprise.Application.Interfaces;
+using USSEnterprise.Domain.Entities;
+
+namespace USSEnterprise.Application.Services
+{
+    public class ElevatorService : IElevatorService
+    {
+        private readonly Elevator _elevator;
+
+        public ElevatorService(Elevator elevator)
+        {
+            this._elevator = elevator;
+        }
+
+        public void RequestFloor(int floor)
+        {
+            
+        }
+    }
+}
